9bbb2239ed8f51bef8ab5b6837df142fbb479e92
dongsen2019 <57001543+dongsen2019@users.noreply.github.com>
2020-01-31 17:08:32 +0800

    Create DLL_QUEUE.h

---
+++ DLL_QUEUE.h
@@ -0,0 +1,38 @@
+//DLL_QUEUE.h
+#ifndef DLL_QUEUE
+#define DLL_QUEUE
+
+#include <list> 
+using namespace std;
+
+template<typename T>
+class DDLQueue 
+{
+	private:
+		list<T> que;
+		T * head;
+		T * tail;
+	public:
+		DLLQueue() {
+			head = tail = 0;
+		}
+		void clear() {
+			que.clear();
+		}
+		bool isEmpty() {
+			return que.empty();
+		}
+		void enqueue(const T & el) {
+			que.push_back(el);
+		}
+		T dequeue() {
+			T temp = que.front();
+			que.pop_front();
+			return temp;
+		}
+		T firstEl() {
+			return que.front();
+		}	
+}; 
+
+#endif 
